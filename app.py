7a74f17e8ffe9ca8e88545053d2966b2c5f23aab
cherilynss <cherilyn.susanto@my.jcu.edu.au>
2023-05-22 11:39:59 +0800

    Define a function to train the face recognition model

--- app.py
+++ app.py
@@ -42,3 +42,18 @@ def extract_faces(img):
 def identify_face(facearray):
     model = joblib.load('static/face_recognition_model.pkl')
     return model.predict(facearray)
+
+def train_model():
+    faces = []
+    labels = []
+    userlist = os.listdir('static/faces')
+    for user in userlist:
+        for imgname in os.listdir(f'static/faces/{user}'):
+            img = cv2.imread(f'static/faces/{user}/{imgname}')
+            resized_face = cv2.resize(img, (50, 50))
+            faces.append(resized_face.ravel())
+            labels.append(user)
+    faces = np.array(faces)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(faces, labels)
+    joblib.dump(knn, 'static/face_recognition_model.pkl')
