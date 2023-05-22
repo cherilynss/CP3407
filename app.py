682ac57e287fdad42d85909682019d26352b42da
cherilynss <cherilyn.susanto@my.jcu.edu.au>
2023-05-22 11:34:48 +0800

    Create necessary directories if they don't exist, Create a CSV file for attendance if it doesn't exist, Define the current date and date formats

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import cv2
+import os
+from datetime import date
 from flask import Flask
 
 app = Flask(__name__)
+
+datetoday = date.today().strftime("%m_%d_%y")
+datetoday2 = date.today().strftime("%d-%B-%Y")
+
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -9,3 +15,15 @@ try:
     cap = cv2.VideoCapture(1)
 except:
     cap = cv2.VideoCapture(0)
+
+if not os.path.isdir('Attendance'):
+    os.makedirs('Attendance')
+if not os.path.isdir('static'):
+    os.makedirs('static')
+if not os.path.isdir('static/faces'):
+    os.makedirs('static/faces')
+
+if f'Attendance-{datetoday}.csv' not in os.listdir('Attendance'):
+    with open(f'Attendance/Attendance-{datetoday}.csv', 'w') as f:
+        f.write('Name,Roll,Time')
+
